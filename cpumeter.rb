9a3d74dd44057d8e0573ce3b8524da1170e0bca8
FATA.IO <owner@fata.io>
2015-06-05 17:44:54 +0900

    for brew cask

--- cpumeter.rb
+++ cpumeter.rb
@@ -2,12 +2,10 @@ cask :v1 => 'cpumeter' do
   version '1.0.3'
   sha256 '078936bffa150083217f0f171522c0dfafb42b3a03e6c7ebd8295bb57753cc2a'
 
-  url 'https://github.com/fataio/cpumeter/blob/master/dist/cpumeter.pkg?raw=true'
+  url 'https://github.com/fataio/cpumeter/blob/master/dist/cpumeter.zip?raw=true'
   name 'cpumeter'
   homepage 'https://github.com/fataio/cpumeter/'
   license :oss
 
-  pkg 'cpumeter.pkg'
-
-  uninstall :pkgutil => 'io.fata.app.cpumeter'
+  app 'cpumeter.app'
 end
