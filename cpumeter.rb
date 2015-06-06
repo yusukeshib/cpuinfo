14945f2c894964036ce9adb74749e8407f67ea31
FATA <shibata@fata.io>
2015-06-07 08:17:51 +0900

    brew cask rb

--- cpumeter.rb
+++ cpumeter.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'cpumeter' do
-  version '1.0.3'
-  sha256 'fbc7ce7aa2a744909919542dcf6018e3009906a34c239e5a7678536f4668341a'
+  version :latest
+  sha256 :no_check
 
   url 'https://github.com/fataio/cpumeter/blob/master/dist/cpumeter.zip?raw=true'
   name 'cpumeter'
   homepage 'https://github.com/fataio/cpumeter/'
-  license :oss
+  license :mit
 
   app 'cpumeter.app'
 end
